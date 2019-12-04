9bd82e8e52a30eba1cbe4661cc55198066779441
Kevin Geng <khg@andrew.cmu.edu>
2019-12-04 00:43:26 -0500

    Foolish template changes

--- template.py
+++ template.py
@@ -6,38 +6,16 @@ from collections import Counter, defaultdict, deque
 from itertools import permutations, combinations, product
 import aocd
 
+
 is_sample = False
 DAY = # TODO: change me
 YEAR = 2019
 
-def submit_1(res):
-    print('Part 1', res)
-    if not is_sample and not input("skip? "):
-        puzzle.answer_a = res
 
-
-def submit_2(res):
-    print('Part 2', res)
-    if not is_sample and not input("skip? "):
-        puzzle.answer_b = res
-
-
-def parse_ints(text):
-    "All the integers anywhere in text."
-    return [int(x) for x in re.findall(r'[-+]?\d+', text)]
-
-
-def parse_line(line):
-    #return line.split()
-    #return parse_ints(line)
-    #return line
-    #return int(line)
+##########################################################################
 
 
 def main(A):
-    A = A.splitlines()
-    A = [parse_line(line) for line in A]
-
     # Solve part 1
     def part1():
         return 0
@@ -53,6 +31,58 @@ def main(A):
     submit_2(res)
 
 
+##########################################################################
+
+
+def parse_token(x):
+    #x = int(x)
+    return x
+
+
+def parse_line(line):
+    # One token per line
+    line = parse_token(line)
+
+    # Multiple tokens per line
+    #line = line.split()
+    #line = re.findall(r'\d+', line)
+    #line = re.findall(r'[-+]?\d+', line)
+    #line = [parse_token(x) for x in line]
+
+    return line
+
+
+def parse_input(A):
+    A = A.strip()
+
+    # One line per input
+    A = parse_line(A)
+
+    # Multiple lines per input
+    #A = A.splitlines()
+    #A = [parse_line(line) for line in A]
+
+    return A
+
+
+##########################################################################
+
+
+def submit_1(res):
+    print('Part 1', res)
+    if not is_sample and not input("skip? "):
+        puzzle.answer_a = res
+
+
+def submit_2(res):
+    print('Part 2', res)
+    if not is_sample and not input("skip? "):
+        puzzle.answer_b = res
+
+
+##########################################################################
+
+
 if __name__ == '__main__':
     if len(sys.argv) >= 2 and sys.argv[1].startswith('s'):
         A = open('sample.txt').read()
@@ -60,4 +90,4 @@ if __name__ == '__main__':
     else:
         puzzle = aocd.models.Puzzle(day=DAY, year=YEAR)
         A = puzzle.input_data
-    main(A)
+    main(parse_input(A))
